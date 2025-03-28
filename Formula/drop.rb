baf4c2f5c54b67c16ac86ef2c2a91c52df67d8a0
lucasepe <luca.sepe@gmail.com>
2025-03-28 11:06:44 +0100

    feat: bump drop version to v0.4.0

--- Formula/drop.rb
+++ Formula/drop.rb
@@ -1,20 +1,20 @@
 class Drop < Formula
   desc "Lightweight and secure HTTP server for hosting static files from a specified directory"
   homepage "https://cli-tools.dev/posts/drop/"
-  version "0.3.0"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lucasepe/drop/releases/download/v0.3.0/drop-macos-arm64.zip"
-      sha256 "43a04bc1996bd47353f42d00729546bcb75ceb0c0587429a948af2856663addb"
+      url "https://github.com/lucasepe/drop/releases/download/v0.4.0/drop-macos-arm64.zip"
+      sha256 "a57ea3547685ea3a3d091e6811627cc2d97907f7454d93cf5b777135a48fb0b7"
 
       def install
         bin.install "drop"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lucasepe/drop/releases/download/v0.3.0/drop-macos-x86_64.zip"
-      sha256 "cd6ac802de28089cb85221f8f184ab29b39a64154638c032443b7764b8de5ac6"
+      url "https://github.com/lucasepe/drop/releases/download/v0.4.0/drop-macos-x86_64.zip"
+      sha256 "554d335ce9f221168dd939d363740c486009eb2965731906132f94b0f9a31106"
 
       def install
         bin.install "drop"
